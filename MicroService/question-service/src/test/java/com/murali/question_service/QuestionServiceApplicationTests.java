2e9955ae7b38051daf13e4b6f8438dc95d264d87
G MURALI YADAV <113508434+gmuraliyadav@users.noreply.github.com>
2024-08-12 17:36:11 +0530

    Add files via upload

---
+++ MicroService/question-service/src/test/java/com/murali/question_service/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murali.question_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
